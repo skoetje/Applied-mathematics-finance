335b6c49723356f1e44c66bfa87712d4cea6829b
Skoetje <b.e.zijlstra@students.uu.nl>
2016-02-26 22:05:14 +0100

    l Added optionPrice. randoWalk.m

---
+++ Random Walk/optionPrice.m
@@ -0,0 +1,11 @@
+function thePrice = optionPrice(aS, aMu, aSigma, aE, aT)
+    % Return:
+    %  thePrice : double - the price of an option with given parameters.
+    % Parameters:
+    %  aS     : double - start value
+    %  aMu    : double - drift per year
+    %  aSigma : double - volatility per year
+    %  aE     : double - the exercise price
+    %  aT     : double - time until expiry in years
+    thePrice = 0;
+end
---
+++ Random Walk/randomWalk.m
@@ -0,0 +1,10 @@
+function theRandomWalk = randomWalk(aS, aMu, aSigma, aT)
+    % Return:
+    %  theRandomWalk : n x 1 double - one random walk of an asset value
+    % Parameters:
+    %  aS     : double - start value
+    %  aMu    : double - drift per year
+    %  aSigma : double - volatility per year
+    %  aT     : double - time until expiry in years
+    theRandomWalk = [aS];
+end
