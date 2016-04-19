938d2d31b493d7475e10092db037cae296b43c70
Skoetje <b.e.zijlstra@students.uu.nl>
2016-04-19 22:58:45 +0200

    A completely working abitrage bot

--- 3.statistical-arbitrage/ExerciseCor/Template/AnalysisRobot.m
+++ 3.statistical-arbitrage/ExerciseCor/Template/AnalysisRobot.m
@@ -2,6 +2,9 @@ classdef AnalysisRobot < AutoTrader
     properties
         CBKDepth
         DBKDepth
+        
+        % Best bids/offer prices and volumes of both stocks are recorded to
+        % use these values in the valuation functions later
         BestBidsCBK
         BestBidsDBK
         BestOffersCBK
@@ -18,6 +21,8 @@ classdef AnalysisRobot < AutoTrader
                 case 'CBK_EUR'; aBot.CBKDepth = aDepth;
                 case 'DBK_EUR'; aBot.DBKDepth = aDepth;
             end
+            
+            % Recording of best bid and offer volumes and prices
             myCBK=aBot.CBKDepth;
             myDBK=aBot.DBKDepth;
             myCBKbid=NaN;
@@ -31,24 +36,20 @@ classdef AnalysisRobot < AutoTrader
             if (isempty(myCBK)==0),
                 if (isempty(myCBK.bidLimitPrice)==0),
                     myCBKbid=myCBK.bidLimitPrice(1);
-                    myCBKbidvol=sum(myCBK.bidVolume);
                     myCBKbidvol=myCBK.bidVolume(1);
                 end
                 if (isempty(myCBK.askLimitPrice)==0),
                     myCBKoffer=myCBK.askLimitPrice(1);
-                    myCBKoffervol=sum(myCBK.askVolume);
                     myCBKoffervol=myCBK.askVolume(1);
                 end
             end
             if (isempty(myDBK)==0),
                 if (isempty(myDBK.bidLimitPrice)==0),
                     myDBKbid=myDBK.bidLimitPrice(1);
-                    myDBKbidvol=sum(myDBK.bidVolume);
                     myDBKbidvol=myDBK.bidVolume(1);
                 end
                 if (isempty(myDBK.askLimitPrice)==0),
                     myDBKoffer=myDBK.askLimitPrice(1);
-                    myDBKoffervol=sum(myDBK.askVolume);
                     myDBKoffervol=myDBK.askVolume(1);
                 end
             end
@@ -63,95 +64,31 @@ classdef AnalysisRobot < AutoTrader
         end
         
         function PlotMarket(aBot)
-            %Get best bids and offers
-            myTime=[1:length(aBot.BestBidsCBK)];
-            myBidCBK = aBot.BestBidsCBK(~isnan(aBot.BestBidsCBK));
-            myAskCBK = aBot.BestOffersCBK(~isnan(aBot.BestOffersCBK));
-            myBidDBK = aBot.BestBidsDBK(~isnan(aBot.BestBidsDBK));
-            myAskDBK = aBot.BestOffersDBK(~isnan(aBot.BestOffersDBK));
+            % Get best bids and offers
+            myTime=1:length(aBot.BestBidsCBK);
             
-            %Valuate stocks
+            % Valuate stocks
             myDBKvalue=Valuate(aBot.BestOffersDBK,aBot.BestVolOffersDBK,aBot.BestBidsDBK,aBot.BestVolBidsDBK,0);
             myCBKvalue=Valuate(aBot.BestOffersCBK,aBot.BestVolOffersCBK,aBot.BestBidsCBK,aBot.BestVolBidsCBK,0);
             myDBKvalue2=myDBKvalue(~isnan(myDBKvalue));
             myCBKvalue2=myCBKvalue(~isnan(myCBKvalue));
             
-            %Calculate correlation coefficient
+            % Calculate correlation coefficient
             myCorrelation=corrcoef(myDBKvalue,myCBKvalue,'rows','complete');
             myCorrelation=myCorrelation(2,1);
-                        
-            %Principal component Analysis
-            [eigenvec, eigenval]=eig(corrcoef(myDBKvalue,myCBKvalue,'rows','complete'));
-            myPC1=eigenval(1,1)/(eigenval(1,1)+eigenval(2,2));
-            myPC2=eigenval(2,2)/(eigenval(1,1)+eigenval(2,2));
-            myPC1a=eigenvec(1,1);
-            myPC1b=eigenvec(1,2);
-            myPC2a=eigenvec(2,1);
-            myPC2b=eigenvec(2,2);
+          
+            % Linear relation
+            myConfidence=0.05;
+            myNonNanValues = isnan(myCBKvalue) | isnan(myDBKvalue);
+            myParameters = polyfit(myCBKvalue(~myNonNanValues),myDBKvalue(~myNonNanValues),1);
+            [a,b] = polyfit(myCBKvalue(~myNonNanValues),myDBKvalue(~myNonNanValues),1);
+            [myRegression0,myDelta0]=polyconf(a,myCBKvalue,b,'alpha',myConfidence);
+            myRegressionfit = polyval(a,myCBKvalue);
             
-            myPCAVALUE=myPC1a*myDBKvalue+myPC1b*myCBKvalue;
-            myPCAVALUE2=myPC2a*myDBKvalue+myPC2b*myCBKvalue;
-            myTradePredictor=-myPC1b*myCBKvalue/myPC1a+nanmean(myPCAVALUE)/myPC1a;
-            myTradePredictor2=myTradePredictor(~isnan(myTradePredictor));
-            myDifference=myDBKvalue-myTradePredictor;
-            myDifference2=myDifference(~isnan(myDifference));
-            %length(myTradePredictor)
-            %myPC1b/myPC1a
-            
-            %Check for divergence or convergence
-            
-            %Linear relation
-            alpha=0.05;
-            idx = isnan(myCBKvalue) | isnan(myDBKvalue);
-            myParameters = polyfit(myCBKvalue(~idx),myDBKvalue(~idx),1);
-            [a,b] = polyfit(myCBKvalue(~idx),myDBKvalue(~idx),1);
-            Regr= polyfit(myCBKvalue(~idx),myDBKvalue(~idx),1);
-            [Y,DELTA]=polyconf(a,myCBKvalue,b,'alpha',alpha);
-            yfit = polyval(a,myCBKvalue);
-            %hfit = polyval(p,xfit);
-            
+            % Calculation of the actual regression (for the used feed)
             myRegression = myParameters(1)*myCBKvalue+myParameters(2);
-            myRegrError = sqrt(diag(inv(b.R)*inv(b.R'))./b.normr.^2./b.df);
-            myRegressionUp = (myParameters(1)+myRegrError(1))*myCBKvalue+myParameters(2)+myRegrError(2);
-            myRegressionDown = (myParameters(1)-myRegrError(1))*myCBKvalue+myParameters(2)-myRegrError(2);
             
-            myRegrDifference = myDBKvalue(~idx)-myRegression(~idx);
-            myRegrDifferenceUp = myDBKvalue(~idx)-myRegressionUp(~idx);
-            myRegrDifferenceDown = myDBKvalue(~idx)-myRegressionDown(~idx);
-            
-            %Make plotss
-            %subplot(2,2,1);
-%             hold on
-%             yyaxis left
-%             plot(myTime(~isnan(aBot.BestBidsCBK)),myBidCBK,'-')
-%             plot(myTime(~isnan(aBot.BestOffersCBK)),myAskCBK,'-','Color','blue')
-%             ylabel('Best offer and bid prices')
-%             text(100,(max(myAskCBK)-min(myBidCBK))*0.95+min(myBidCBK),strcat('r_{Valuations} = ',num2str(myCorrelation)))
-%             text(100,(max(myAskCBK)-min(myBidCBK))*0.85+min(myBidCBK),strcat('PC1 = ',num2str(myPC1)))
-%             text(100,(max(myAskCBK)-min(myBidCBK))*0.75+min(myBidCBK),strcat('PC1 = ',num2str(myPC1a),'*DBKvalue +',num2str(myPC1b),'*DBKvalue'))
-%             text(100,(max(myAskCBK)-min(myBidCBK))*0.65+min(myBidCBK),strcat('PC2 = ',num2str(myPC2)))
-%             text(100,(max(myAskCBK)-min(myBidCBK))*0.55+min(myBidCBK),strcat('PC2 = ',num2str(myPC2a),'*CBKvalue +',num2str(myPC2b),'*CBKvalue'))
-%             yyaxis right
-%             plot(myTime(~isnan(myCBKvalue)),myCBKvalue2,'-','LineWidth',2)
-%             ylabel('Valuation')
-%             xlabel('Time')
-%             title('CBK')
-%             hold off
-            
-            %subplot(2,2,2);
-%             hold on
-%             yyaxis left
-%             plot(myTime(~isnan(aBot.BestBidsDBK)),myBidDBK,'-')
-%             plot(myTime(~isnan(aBot.BestOffersDBK)),myAskDBK,'-','Color','blue')
-%             ylabel('Best offer and bid prices')
-%             %text(100,15.75,strcat('r_{CBKDBK} = ',num2str(myCorrelation)))
-%             yyaxis right
-%             plot(myTime(~isnan(myDBKvalue)),myDBKvalue2,'-','LineWidth',2)
-%             ylabel('Valuation')
-%             xlabel('Time')
-%             title('DBK')
-%             hold off
-            
+            % Plots of various sorts with various quantities
             subplot(2,2,1);
             hold on
             yyaxis left
@@ -168,38 +105,28 @@ classdef AnalysisRobot < AutoTrader
             plot(myTime(~isnan(myRegression)),myRegression(~isnan(myRegression)),'-','Color','blue')
             plot(myTime(~isnan(myDBKvalue)),myDBKvalue(~isnan(myDBKvalue)),'-')
             text(100,(max(myDBKvalue)-min(myDBKvalue))*0.95+min(myDBKvalue),strcat('DBK = ',num2str(myParameters(1)),'x + ',num2str(myParameters(2))))
-            text(100,(max(myDBKvalue)-min(myDBKvalue))*0.85+min(myDBKvalue),strcat('Unc1 = ',num2str(myRegrError(1))))
-            text(100,(max(myDBKvalue)-min(myDBKvalue))*0.75+min(myDBKvalue),strcat('Unc2 = ',num2str(myRegrError(2))))
             title('Regression')
             hold off
             
             subplot(2,2,3);
             hold on
             yyaxis left
-            %plot(myTime(~isnan(myRegrDifference)),myRegrDifference(~isnan(myRegrDifference)),'-','Color','black')
-            %plot(myTime(~isnan(myRegrDifferenceUp)),myRegrDifferenceUp(~isnan(myRegrDifferenceUp)),'-','Color','blue')
-            %plot(myTime(~isnan(myRegrDifferenceDown)),myRegrDifferenceDown(~isnan(myRegrDifferenceDown)),'-','Color','red')
-            y1=Y+DELTA;
-            y2=Y-DELTA;
-            plot(myTime(~isnan(y1)),y1(~isnan(y1)),'b-');
-            plot(myTime(~isnan(y2)),y2(~isnan(y2)),'b-')
-            plot(myTime(~isnan(yfit)),yfit(~isnan(yfit)),'b-')
+            myHighRegression=myRegression0+myDelta0;
+            myLowRegression=myRegression0-myDelta0;
+            plot(myTime(~isnan(myHighRegression)),myHighRegression(~isnan(myHighRegression)),'b-');
+            plot(myTime(~isnan(myLowRegression)),myLowRegression(~isnan(myLowRegression)),'b-')
+            plot(myTime(~isnan(myRegressionfit)),myRegressionfit(~isnan(myRegressionfit)),'b-')
             plot(myTime(~isnan(myDBKvalue)),myDBKvalue(~isnan(myDBKvalue)),'-')
-            text(100,(max(myDBKvalue)-min(myDBKvalue))*0.75+min(myDBKvalue),strcat('Delta =',num2str(nanmean(DELTA))))
-            title(strcat('Regression and  ',num2str(100*(1.0-alpha)),'% Confidence intervals'))
+            text(100,(max(myDBKvalue)-min(myDBKvalue))*0.75+min(myDBKvalue),strcat('Delta =',num2str(nanmean(myDelta0))))
+            title(strcat('Regression and  ',num2str(100*(1.0-myConfidence)),'% Confidence intervals'))
             hold off
             
             subplot(2,2,4);
             hold on
             yyaxis left
-            plot(myTime(~isnan(myRegressionUp)),myRegressionUp(~isnan(myRegressionUp)),'-','Color','blue')
-            plot(myTime(~isnan(myRegressionDown)),myRegressionDown(~isnan(myRegressionDown)),'-','Color','red')
             plot(myTime(~isnan(myRegression)),myRegression(~isnan(myRegression)),'-','Color','black')
-            text(100,(max(myRegressionUp)-min(myRegressionUp))*0.95+min(myRegressionUp),strcat('r_{Valuations} = ',num2str(myCorrelation)))
             title('Regression Difference')
             hold off
-            
-            
         end
     end
 end
--- 3.statistical-arbitrage/ExerciseCor/Template/TradingRobot.m
+++ 3.statistical-arbitrage/ExerciseCor/Template/TradingRobot.m
@@ -2,6 +2,21 @@ classdef TradingRobot < AutoTrader
     properties
         CBKDepth
         DBKDepth
+        
+        %TotalCBK and DBK are to gain insight in current position
+        TotalCBK
+        TotalDBK
+        
+        %The ask- and bidprices per stock are for the part of the unwind
+        %function that will check what the worst ask/bid prices were during
+        %the whole feed (this is optional)
+        AskPricesCBK
+        BidPricesCBK
+        AskPricesDBK
+        BidPricesDBK
+        
+        %Time is to know current time in the feed
+        Time
     end
 
     methods
@@ -12,49 +27,289 @@ classdef TradingRobot < AutoTrader
             end
             
             aBot.TryArbitrage();
+        end
+        
+        function Unwind(aBot)
+        
+        % Reduce final position using final books
+        % First get final position:    
+        myTotalCBK=sum(aBot.TotalCBK);
+        myTotalDBK=sum(aBot.TotalDBK);
+        
+        % Now check available trades and send them if they reduce position:
+        for k=1:length(aBot.CBKDepth.bidVolume),
+            if myTotalCBK>0 && isempty(aBot.CBKDepth.bidVolume)==0,
+                myBidVolume=min(myTotalCBK,aBot.CBKDepth.bidVolume(1));
+                myBidPrice=aBot.CBKDepth.bidLimitPrice(1);
+                aBot.SendNewOrder(myBidPrice, myBidVolume,  -1, {'CBK_EUR'}, {'IMMEDIATE'}, 0);  
+                myTotalCBK=myTotalCBK-myBidVolume;
+            end
+        end
+        for k=1:length(aBot.CBKDepth.askVolume),
+            if myTotalCBK<0 && isempty(aBot.CBKDepth.askVolume)==0,
+                myAskVolume=min(abs(myTotalCBK),aBot.CBKDepth.askVolume(1));
+                myAskPrice=aBot.CBKDepth.askLimitPrice(1);
+                aBot.SendNewOrder(myAskPrice, myAskVolume,  1, {'CBK_EUR'}, {'IMMEDIATE'}, 0);
+                myTotalCBK=myTotalCBK+myAskVolume;
+            end
+        end
+        for k=1:length(aBot.DBKDepth.bidVolume),
+            if myTotalDBK>0 && isempty(aBot.DBKDepth.bidVolume)==0,
+                myBidVolume=min(myTotalDBK,aBot.DBKDepth.bidVolume(1));
+                myBidPrice=aBot.DBKDepth.bidLimitPrice(1);
+                aBot.SendNewOrder(myBidPrice, myBidVolume,  -1, {'DBK_EUR'}, {'IMMEDIATE'}, 0);   
+                myTotalDBK=myTotalDBK-myBidVolume;
+            end
+        end
+        for k=1:length(aBot.DBKDepth.askVolume),
+            if myTotalDBK<0 && isempty(aBot.DBKDepth.askVolume)==0,
+                myAskVolume=min(abs(myTotalDBK),aBot.DBKDepth.askVolume(1));
+                myAskPrice=aBot.DBKDepth.askLimitPrice(1);
+                aBot.SendNewOrder(myAskPrice, myAskVolume,  1, {'DBK_EUR'}, {'IMMEDIATE'}, 0);
+                myTotalDBK=myTotalDBK+myAskVolume;
+            end
+        end
+        
+        % Second part: the reduction of final position to zero using worst
+        % case scenario bid/ask prices. This is intentionally left in
+        % comments to gain insight in final position.
+        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         if myTotalDBK<0,
+%             aBot.ownTrades.volume(end+1)=abs(myTotalDBK);
+%             aBot.ownTrades.price(end+1)=max(aBot.AskPricesDBK);     
+%             aBot.ownTrades.side(end+1)=1;       
+%             aBot.ownTrades.ISIN(end+1)={'DBK_EUR'};   
+%             myTotalDBK=myTotalDBK+myTotalDBK;            
+%         end
+%         if myTotalDBK>0,
+%             aBot.ownTrades.volume(end+1)=abs(myTotalDBK);
+%             aBot.ownTrades.price(end+1)=min(aBot.BidPricesDBK);     
+%             aBot.ownTrades.side(end+1)=-1;       
+%             aBot.ownTrades.ISIN(end+1)={'DBK_EUR'}; 
+%             myTotalDBK=myTotalDBK-myTotalDBK;
+%         end
+%         if myTotalCBK<0,
+%             aBot.ownTrades.volume(end+1)=abs(myTotalCBK);
+%             aBot.ownTrades.price(end+1)=max(aBot.AskPricesCBK);     
+%             aBot.ownTrades.side(end+1)=1;       
+%             aBot.ownTrades.ISIN(end+1)={'CBK_EUR'}; 
+%             myTotalCBK=myTotalCBK+myTotalCBK;
+%         end
+%         if myTotalCBK>0,
+%             aBot.ownTrades.volume(end+1)=abs(myTotalCBK);
+%             aBot.ownTrades.price(end+1)=min(aBot.BidPricesCBK);     
+%             aBot.ownTrades.side(end+1)=-1;       
+%             aBot.ownTrades.ISIN(end+1)={'CBK_EUR'}; 
+%             myTotalCBK=myTotalCBK-myTotalCBK;
+%         end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-            function TryArbitrage(aBot)
-                 % Check how many times it should try to trade
-            myCorrelation = 0.6;
-            myPreviousCBKValue = 0;
-            myPreviousDBKValue = 0;
+        end
+        
+        function TryArbitrage(aBot)
+            % Time; only the length of the property is used
+            aBot.Time(end+1)=1;
+            
+            % Regression parameters of the form:
+            % "DBK = MySlope * CBK + myCrossing"
+            % Using the four different feeds
+            mySlope1=2.3969;
+            mySlope2=0.65231;
+            mySlope3=2.9922;
+            mySlope4=3.4283;
+            
+            myCrossing1=-1.4621;
+            myCrossing2=5.2221;
+            myCrossing3=-3.7514;
+            myCrossing4=-5.3519;
+            
+            myR1= 0.59377;
+            myR2=0.31892;
+            myR3=0.8616;
+            myR4=0.81649;
+            
+            myDelta1=0.16725;
+            myDelta2=0.13478;
+            myDelta3=0.13629;
+            myDelta4=0.15749;
+            
+            % Weighted average of regression parameters
+            mySlope=(mySlope1*myR1+mySlope2*myR2+mySlope3*myR3+mySlope4*myR4)/(myR1+myR2+myR3+myR4);
+            myCrossing=(myCrossing1*myR1+myCrossing2*myR2+myCrossing3*myR3+myCrossing4*myR4)/(myR1+myR2+myR3+myR4);
+            myDelta=(myDelta1*myR1+myDelta2*myR2+myDelta3*myR3+myDelta4*myR4)/(myR1+myR2+myR3+myR4);
+                        
+            % Check how many times it should try to trade (this is to check
+            % multiple layers)
             a=0;
             b=0;
             c=0;
             d=0;
             if (isempty(aBot.CBKDepth)==0),
                 a=length(aBot.CBKDepth.askVolume);
+                if (isempty(aBot.CBKDepth.askVolume)==0),
+                    aBot.AskPricesCBK(end+1)=max(aBot.CBKDepth.askLimitPrice);
+                end
             end
             if (isempty(aBot.DBKDepth)==0),
                 b=length(aBot.DBKDepth.askVolume);
+                if (isempty(aBot.DBKDepth.askVolume)==0),
+                    aBot.AskPricesDBK(end+1)=max(aBot.DBKDepth.askLimitPrice);
+                end
             end
             if (isempty(aBot.CBKDepth)==0),
                 c=length(aBot.CBKDepth.bidVolume);
+                if (isempty(aBot.CBKDepth.bidVolume)==0),
+                    aBot.BidPricesCBK(end+1)=min(aBot.CBKDepth.bidLimitPrice);
+                end
             end
             if (isempty(aBot.DBKDepth)==0),
                 d=length(aBot.DBKDepth.bidVolume);
+                if (isempty(aBot.DBKDepth.bidVolume)==0),
+                    aBot.BidPricesDBK(end+1)=min(aBot.DBKDepth.bidLimitPrice); 
+                end
             end
             
+            % Now define some parameters for the Delta functions:
+            myTotalTime=3417.; % Approximation of total time
+            myChangepoint=0.5; % Where to change functions
+            myKappa=0.25;      % Determines start delta
+            myTimeDelta1st=myDelta*sin(2*pi*sum(aBot.Time)/(myTotalTime*4*myChangepoint));
+            myTimeDelta2nd=myDelta*(abs(sum(aBot.Time)-myTotalTime*myChangepoint)/myTotalTime);%sin(2*pi*(sum(aBot.Time)-TotalTime/2)/(TotalTime*2));
+            
+            % Now define Delta for the first half...
+            if sum(aBot.Time)<(myTotalTime*myChangepoint),
+                myDeltaBuyCBK=(1-myKappa)*myTimeDelta1st+myKappa*myDelta;
+                myDeltaSellCBK=(1-myKappa)*myTimeDelta1st+myKappa*myDelta;
+                myDeltaBuyDBK=(1-myKappa)*myTimeDelta1st+myKappa*myDelta;
+                myDeltaSellDBK=(1-myKappa)*myTimeDelta1st+myKappa*myDelta;
+                
+            % ... and the second half:
+            else
+                myLowDeltaCBK=myDelta-myTimeDelta2nd*(1+abs(sum(aBot.TotalCBK))/50);
+                myHighDeltaCBK=myDelta+myTimeDelta2nd*(1+abs(sum(aBot.TotalCBK))/50);
+                myLowDeltaDBK=myDelta-myTimeDelta2nd*(1+abs(sum(aBot.TotalDBK))/50);
+                myHighDeltaDBK=myDelta+myTimeDelta2nd*(1+abs(sum(aBot.TotalDBK))/50);
+                
+                % Here we check the different cases of positions
+                if sum(aBot.TotalCBK)<0,
+                    myDeltaBuyCBK=myLowDeltaCBK;
+                    myDeltaSellCBK=myHighDeltaCBK;
+                end
+
+                if sum(aBot.TotalDBK)<0,
+                    myDeltaBuyDBK=myLowDeltaDBK;
+                    myDeltaSellDBK=myHighDeltaDBK;
+                end
+
+                if sum(aBot.TotalCBK)>=0,
+                    myDeltaBuyCBK=myHighDeltaCBK;
+                    myDeltaSellCBK=myLowDeltaCBK;
+                end
+
+                if sum(aBot.TotalDBK)>=0,
+                    myDeltaBuyDBK=myHighDeltaDBK;
+                    myDeltaSellDBK=myLowDeltaDBK;
+                end
+            end
+            
+            % Now onto the trading!
             for k = 1:max([a,b,c,d]),
                 myCBK=aBot.CBKDepth;
-                myDBK=aBot.DBKDepth;
+                myDBK=aBot.DBKDepth;                  
                 if (isempty(myCBK)==0)&&(isempty(myDBK)==0),
                     if (isempty(myCBK.bidVolume)==0) && (isempty(myCBK.askVolume)==0) &&(isempty(myDBK.bidVolume)==0) && (isempty(myDBK.askVolume)==0),
-                        myCBKValue=Valuate(myCBK.askLimitPrice(1),myCBK.askVolume,myCBK.bidLimitPrice(1),myCBK.bidVolume,0);
-                        myDBKValue=Valuate(myDBK.askLimitPrice(1),myDBK.askVolume,myDBK.bidLimitPrice(1),myDBK.bidVolume,0);
+                        myCBKvalue=Valuate(myCBK.askLimitPrice(1),myCBK.askVolume,myCBK.bidLimitPrice(1),myCBK.bidVolume,0);
+                        myDBKvalue=Valuate(myDBK.askLimitPrice(1),myDBK.askVolume,myDBK.bidLimitPrice(1),myDBK.bidVolume,0);
                         
-                        % Here we check the possibility for arbitrage
+                        % Check mispricing
+                        if myDBKvalue>(mySlope)*myCBKvalue+myCrossing+min(myDeltaBuyCBK,myDeltaSellDBK),
+                            myAskPrice = myCBK.askLimitPrice(1);
+                            myBidPrice = myDBK.bidLimitPrice(1);
+                            myAskVolume = min(myCBK.askVolume(1),myCBK.bidVolume(1));
+                            myBidVolume = min(myDBK.bidVolume(1),myDBK.askVolume(1));
+
+                            % Update book
+                            aBot.DBKDepth.bidVolume(1) = aBot.DBKDepth.bidVolume(1)-myBidVolume;
+                            aBot.CBKDepth.askVolume(1) = aBot.CBKDepth.askVolume(1)-myAskVolume;
+                            
+                            % Now specifically check mispricing of stocks
+                            % using the Delta specified for this trade
+                            if myDBKvalue>(mySlope)*myCBKvalue+myCrossing+myDeltaBuyCBK,           
+                                aBot.TotalCBK(end+1)=myAskVolume*1;
+                                aBot.SendNewOrder(myAskPrice, myAskVolume,  1, {'CBK_EUR'}, {'IMMEDIATE'}, 0);
+                            end
+                            
+                            % Again check mispricing using specific delta
+                            if myDBKvalue>(mySlope)*myCBKvalue+myCrossing+myDeltaSellDBK,     
+                                aBot.TotalDBK(end+1)=myBidVolume*-1;
+                                aBot.SendNewOrder(myBidPrice, myBidVolume, -1, {'DBK_EUR'}, {'IMMEDIATE'}, 0);
+                            end
+
+                            % Update layers to make the program check if
+                            % another layer trade is also viable
+                            if aBot.DBKDepth.bidVolume(1) ==0,
+                                for i=1:(length(aBot.DBKDepth.bidVolume)-1),
+                                    aBot.DBKDepth.bidVolume(i)=aBot.DBKDepth.bidVolume(i+1);
+                                    aBot.DBKDepth.bidLimitPrice(i)=aBot.DBKDepth.bidLimitPrice(i+1);
+                                end
+                                aBot.DBKDepth.bidVolume(end)=[];%0;
+                                aBot.DBKDepth.bidLimitPrice(end)=[];%0.1;
+                            end
+                            if aBot.CBKDepth.askVolume(1) ==0,
+                                for i=1:(length(aBot.CBKDepth.askVolume)-1),
+                                    aBot.CBKDepth.askVolume(i)=aBot.CBKDepth.askVolume(i+1);
+                                    aBot.CBKDepth.askLimitPrice(i)=aBot.CBKDepth.askLimitPrice(i+1);
+                                end
+                                aBot.CBKDepth.askVolume(end)=[];%0;
+                                aBot.CBKDepth.askLimitPrice(end)=[];%1000000;
+                            end
+                        end
                         
-                end
+                        % Check for mispricing
+                        if myDBKvalue<(mySlope)*myCBKvalue+myCrossing-min(myDeltaBuyDBK,myDeltaSellCBK),
+                            myAskPrice = myDBK.askLimitPrice(1);
+                            myBidPrice = myCBK.bidLimitPrice(1);
+                            myAskVolume = min(myDBK.askVolume(1),myDBK.bidVolume(1));
+                            myBidVolume = min(myCBK.bidVolume(1),myCBK.askVolume(1));
+
+                            % Update book
+                            aBot.CBKDepth.bidVolume(1) = aBot.CBKDepth.bidVolume(1)-myBidVolume;
+                            aBot.DBKDepth.askVolume(1) = aBot.DBKDepth.askVolume(1)-myAskVolume;
+                            
+                            % Again check mispricing using specific delta
+                            if myDBKvalue<(mySlope)*myCBKvalue+myCrossing-myDeltaBuyDBK,  
+                                aBot.TotalDBK(end+1)=myAskVolume*1;
+                                aBot.SendNewOrder(myAskPrice, myAskVolume,  1, {'DBK_EUR'}, {'IMMEDIATE'}, 0);
+                            end
+                            % Again check mispricing using specific delta
+                            if myDBKvalue<(mySlope)*myCBKvalue+myCrossing-myDeltaSellCBK,  
+                                aBot.TotalCBK(end+1)=myBidVolume*-1;
+                                aBot.SendNewOrder(myBidPrice, myBidVolume, -1, {'CBK_EUR'}, {'IMMEDIATE'}, 0);
+                            end
+
+                            % Update layers to make the program check if
+                            % another layer trade is also viable
+                            if aBot.CBKDepth.bidVolume(1) ==0,
+                                for i=1:(length(aBot.CBKDepth.bidVolume)-1),
+                                    aBot.CBKDepth.bidVolume(i)=aBot.CBKDepth.bidVolume(i+1);
+                                    aBot.CBKDepth.bidLimitPrice(i)=aBot.CBKDepth.bidLimitPrice(i+1);
+                                end
+                                aBot.CBKDepth.bidVolume(end)=[];%0;
+                                aBot.CBKDepth.bidLimitPrice(end)=[];%0.1;
+                            end
+                            if aBot.DBKDepth.askVolume(1) ==0,
+                                for i=1:(length(aBot.DBKDepth.askVolume)-1),
+                                    aBot.DBKDepth.askVolume(i)=aBot.DBKDepth.askVolume(i+1);
+                                    aBot.DBKDepth.askLimitPrice(i)=aBot.DBKDepth.askLimitPrice(i+1);
+                                end
+                                aBot.DBKDepth.askVolume(end)=[];%;
+                                aBot.DBKDepth.askLimitPrice(end)=[];%10000000;
+                            end
+                        end
+                    end
                 end
             end
-            end
-          
-                
-        function Unwind(aBot)
-            %%%% TO DO LATER
-            
-        end
         end
     end
 end
