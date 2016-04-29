c3f46f34ae134fa2eb08009d0b5f67149222e981
Bram <bram.zijlstra@gmail.com>
2016-04-29 23:13:57 +0200

    Reordered files in a nicer wayX

---
+++ 3.statistical-arbitrage/Older functions/old-versions.m
@@ -0,0 +1,21 @@
+The values of the analysis robot on the short feed:
+
+            mySlope1=2.3969;
+            mySlope2=0.65231;
+            mySlope3=2.9922;
+            mySlope4=3.4283;
+            
+            myCrossing1=-1.4621;
+            myCrossing2=5.2221;
+            myCrossing3=-3.7514;
+            myCrossing4=-5.3519;
+            
+            myR1=0.59377;
+            myR2=0.31892;
+            myR3=0.8616;
+            myR4=0.81649;
+            
+            myDelta1=0.16725;
+            myDelta2=0.13478;
+            myDelta3=0.13629;
+            myDelta4=0.15749;
