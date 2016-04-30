da553b3a5d367ba23c835c92191b2315d1bdfdc0
Skoetje <b.e.zijlstra@students.uu.nl>
2016-04-30 13:23:50 +0200

    Added Black-Scholes model, works for 1. Now MC

---
+++ 4.sensitivity-analysis/src/blackScholes.m
@@ -0,0 +1,15 @@
+function Call = blackScholes(Price, Strike, Rate, Time, Volatility, Yield)
+
+Call = blsprice(Price, Strike, Rate, Time, Volatility, Yield);
+
+
+
+% Values for the Black Sholes as in exercise 1
+%%
+%% should return 0.0462 (and you need finacial toolbox)
+% Price = 11;
+% Strike = 12;
+% Rate = 0.02;
+% Time = 82/252;
+% Volatility = 0.12;
+% Yield = 0;
