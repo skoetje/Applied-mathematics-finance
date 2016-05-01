8611d3cab1e344afd745cf1843462afba37e222c
Skoetje <b.e.zijlstra@students.uu.nl>
2016-05-01 14:15:38 +0200

    the tester is working now for one variable

--- 4.sensitivity-analysis/src/tester.m
+++ 4.sensitivity-analysis/src/tester.m
@@ -7,15 +7,17 @@ time = 82/252;
 volatility = 0.12;
 yield = 0;
 
-
 callVector = [];
 putVector = [];
-priceVector =[0:15];
+priceVector =[0:0.01:15];
 for i=1:length(priceVector);
     price = i;
-    [a,b] = blackScholes(i, strike, rate, time, volatility, yield);
-    callVector(end+1)=a;
-    putVector(end+1)=b;
+    %[a, b] = blackScholes(i, strike, rate, time, volatility, yield);
+    [x, y] = blackScholes(12,i,0.02,1,0.12,0);
+    callVector(end+1)=x;
+    putVector(end+1)=y;
+   % callVector(end+1)=a;
+    %putVector(end+1)=b;
 end
 hold on
 plot(priceVector, callVector);
