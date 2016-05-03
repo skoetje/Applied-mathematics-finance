90bf4bf4c4daaf34dc01f3082d850dbb08218395
Meiden <m.m.dekker@outlook.com>
2016-05-03 15:54:08 +0200

    Standard Format

---
+++ 4.sensitivity-analysis/src/InterestRate.m
@@ -0,0 +1,21 @@
+%% Preambule
+mu=0.02;
+sigma=0.2;
+time=82/252;
+yield=0;
+Strike=12;
+Start=11;
+
+%% Plots
+ValueOption=[];
+for i=1:100,
+    mu=0.01+i*0.001;
+    ValueOption(end+1)=blsprice(Start,Strike,mu,time,sigma,yield);
+end
+
+plot(ValueOption)
+%ylim([0,N/20])
+%xlim([0,4])
+xlabel('Drift','FontSize', 15)
+ylabel('Option value (€)','FontSize', 15)
+set(gca,'FontSize',13)
--- 4.sensitivity-analysis/src/blackScholes.m
+++ 4.sensitivity-analysis/src/blackScholes.m
@@ -1,21 +1,5 @@
 function [Call, Put] = blackScholes(Price, Strike, Rate, Time, Volatility, Yield)
     
 [Call, Put] = blsprice(Price, Strike, Rate, Time, Volatility, Yield);
-% disp(Call);
-% disp(Put);
-
-
-
 
 end
-
-
-% Values for the Black Sholes as in exercise 1
-%%
-%% should return 0.0462 (and you need financial toolbox)
-% Price = 11;
-% Strike = 12;
-% Rate = 0.02;
-% Time = 82/252;
-% Volatility = 0.12;
-% Yield = 0;
