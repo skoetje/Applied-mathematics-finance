3aa7509ce8b7ec021f37956fe467be9138b51064
Meiden <m.m.dekker@outlook.com>
2016-05-03 16:07:29 +0200

    Nieuwste Standaard files

--- 4.sensitivity-analysis/src/InterestRate.m
+++ 4.sensitivity-analysis/src/InterestRate.m
@@ -7,15 +7,28 @@ Strike=12;
 Start=11;
 
 %% Plots
-ValueOption=[];
-for i=1:100,
-    mu=0.01+i*0.001;
-    ValueOption(end+1)=blsprice(Start,Strike,mu,time,sigma,yield);
+ValueCall=[];
+ValuePut=[];
+muvec=[];
+for i=1:1000,
+    mu=0.01+i*0.01;
+    [A,B]=blsprice(Start,Strike,mu,time,sigma,yield);
+    ValueCall(end+1)=A;
+    ValuePut(end+1)=B;
+    muvec(end+1)=mu;
 end
-
-plot(ValueOption,'LineWidth',3)
+subplot(2,1,1)
+plot(muvec,ValueCall,'LineWidth',3)
 %ylim([0,N/20])
 %xlim([0,4])
 xlabel('Drift','FontSize', 15)
-ylabel('Option value (€)','FontSize', 15)
+ylabel('Call Option value (€)','FontSize', 15)
+set(gca,'FontSize',13)
+
+subplot(2,1,2)
+plot(muvec,ValuePut,'LineWidth',3)
+%ylim([0,N/20])
+%xlim([0,4])
+xlabel('Drift','FontSize', 15)
+ylabel('Put Option value (€)','FontSize', 15)
 set(gca,'FontSize',13)
