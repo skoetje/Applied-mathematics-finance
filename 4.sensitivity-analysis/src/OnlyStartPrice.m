70263efffc177a51a73e666fe711d48c70981160
Meiden <m.m.dekker@outlook.com>
2016-05-05 10:58:28 +0200

    Further

--- 4.sensitivity-analysis/src/OnlyStartPrice.m
+++ 4.sensitivity-analysis/src/OnlyStartPrice.m
@@ -9,34 +9,36 @@ Start=11;
 %% 1D Plots
 ValueCall=[];
 ValuePut=[];
-muvec=[];
-for i=1:200,
-    sigma=0+i*0.01;
+Startvec=[];
+resolution=0.01;
+
+for i=1:1000,
+    Start=8+i*resolution;
     [A,B]=blsprice(Start,Strike,mu,time,sigma,yield);
     ValueCall(end+1)=A;
     ValuePut(end+1)=B;
-    muvec(end+1)=mu;
+    Startvec(end+1)=Start;
 end
 subplot(2,2,1)
-plot(muvec,ValueCall,'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(Startvec,ValueCall,'LineWidth',3)
+xlabel('Starting Stock Price','FontSize', 15)
 ylabel('Call Option value (€)','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,2)
-plot(muvec,ValuePut,'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(Startvec,ValuePut,'LineWidth',3)
+xlabel('Starting Stock Price','FontSize', 15)
 ylabel('Put Option value (€)','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,3)
-plot(muvec(2:end),diff(ValueCall),'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(Startvec(2:end),diff(ValueCall)/resolution,'LineWidth',3)
+xlabel('Starting Stock Price','FontSize', 15)
 ylabel('Change Value Call option','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,4)
-plot(muvec(2:end),diff(ValuePut),'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(Startvec(2:end),diff(ValuePut)/resolution,'LineWidth',3)
+xlabel('Starting Stock Price','FontSize', 15)
 ylabel('Change Value Call option','FontSize', 15)
 set(gca,'FontSize',13)
--- 4.sensitivity-analysis/src/OnlyVolatility.m
+++ 4.sensitivity-analysis/src/OnlyVolatility.m
@@ -9,34 +9,34 @@ Start=11;
 %% 1D Plots
 ValueCall=[];
 ValuePut=[];
-muvec=[];
+sigmavec=[];
 for i=1:200,
     sigma=0+i*0.01;
     [A,B]=blsprice(Start,Strike,mu,time,sigma,yield);
     ValueCall(end+1)=A;
     ValuePut(end+1)=B;
-    muvec(end+1)=mu;
+    sigmavec(end+1)=sigma;
 end
 subplot(2,2,1)
-plot(muvec,ValueCall,'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(sigmavec,ValueCall,'LineWidth',3)
+xlabel('Volatility rate per year','FontSize', 15)
 ylabel('Call Option value (€)','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,2)
-plot(muvec,ValuePut,'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(sigmavec,ValuePut,'LineWidth',3)
+xlabel('Volatility rate per year','FontSize', 15)
 ylabel('Put Option value (€)','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,3)
-plot(muvec(2:end),diff(ValueCall),'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(sigmavec(2:end),diff(ValueCall),'LineWidth',3)
+xlabel('Volatility rate per year','FontSize', 15)
 ylabel('Change Value Call option','FontSize', 15)
 set(gca,'FontSize',13)
 
 subplot(2,2,4)
-plot(muvec(2:end),diff(ValuePut),'LineWidth',3)
-xlabel('Drift rate per year','FontSize', 15)
+plot(sigmavec(2:end),diff(ValuePut),'LineWidth',3)
+xlabel('Volatility rate per year','FontSize', 15)
 ylabel('Change Value Call option','FontSize', 15)
 set(gca,'FontSize',13)
