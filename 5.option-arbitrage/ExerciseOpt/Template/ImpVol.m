e53b7f8b9ce0d996f6a4f590ffdcf641603bae54
Meiden <m.m.dekker@outlook.com>
2016-05-27 15:03:18 +0200

    Replacing

--- 5.option-arbitrage/ExerciseOpt/Template/ImpVol.m
+++ 5.option-arbitrage/ExerciseOpt/Template/ImpVol.m
@@ -1,3 +1,26 @@
-function theSigmas = ImpVol(aSpots, aStrikes, aTimes, aValues, aIsPuts)
-
+function [volatility] = ImpVol(spotVec,strikeVec,expVec,optionVec,booleVec)
+%Given certain vectors this function retunrs the implied volatility
+    sigmaVec=zeros(length(spotVec),1);
+    looplength = length(spotVec);
+    for i=1:looplength,
+        sigma=0.2;
+        spot=spotVec(i);
+        strike=strikeVec(i);
+        expiry=expVec(i);
+        boolean=booleVec(i);
+        optionv=optionVec(i);
+        value=100;
+        
+        while value-optionVec(i)>0.01,
+            [call, put] = blsprice(spot,strike,0,expiry,sigma,0);
+            if boolean==1,
+                value=call;
+            else
+                value=put;
+            end
+            sigma=sigma+(optionv-value)/blsvega(spot,strike,0,expiry,sigma,0);
+        end
+        sigmaVec(i)=sigma;    
+    end
+    volatility=sigmaVec;
 end
