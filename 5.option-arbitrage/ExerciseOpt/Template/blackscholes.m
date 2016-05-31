d76a9b40e625da376b45c1ff373154cf0578e4a5
Skoetje <b.e.zijlstra@students.uu.nl>
2016-05-31 14:55:30 +0200

    made a black scholes function

---
+++ 5.option-arbitrage/ExerciseOpt/Template/blackscholes.m
@@ -0,0 +1,9 @@
+function [call,put] = blackscholes(S,X,T,r,v)
+% Our own black-scholes formula, without any 
+%   Detailed explanation goes here
+d1=(log(S/X)+(r+v^2/2)*T)/(v*sqrt(T));
+d2=d1-v*sqrt(T);
+call = S*normcdf(d1)-X*exp(-r*T)*normcdf(d2) 
+put = X*exp(-r*T)*normcdf(-d2)-S*normcdf(-d1)
+end
+    
