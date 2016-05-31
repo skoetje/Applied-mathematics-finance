eb7a585b9d5dee31671c8d451471b7a30a7ae94d
Skoetje <b.e.zijlstra@students.uu.nl>
2016-05-31 15:37:15 +0200

    ImpVol works without toolboxes

--- 5.option-arbitrage/ExerciseOpt/Template/ImpVol.m
+++ 5.option-arbitrage/ExerciseOpt/Template/ImpVol.m
@@ -1,4 +1,4 @@
-function [volatility] = ImpVol(spotVec,strikeVec,expVec,optionVec,booleVec)
+function [volatility] = ImpVol(spotVec,strikeVec,timeVec,optionVec,booleVec)
 %Given certain vectors this function retunrs the implied volatility
     sigmaVec=zeros(length(spotVec),1);
     looplength = length(spotVec);
@@ -6,21 +6,21 @@ function [volatility] = ImpVol(spotVec,strikeVec,expVec,optionVec,booleVec)
         sigma=0.2;
         spot=spotVec(i);
         strike=strikeVec(i);
-        expiry=expVec(i);
+        time=timeVec(i);
         boolean=booleVec(i);
         optionv=optionVec(i);
         value=100;
         
         while value-optionVec(i)>0.01,
-            [call, put] = blsprice(spot,strike,0,expiry,sigma,0);
+            [call, put] = blackscholes(spot,strike,time,0,sigma);
             if boolean==1,
                 value=call;
             else
                 value=put;
             end
-            sigma=sigma+(optionv-value)/blsvega(spot,strike,0,expiry,sigma,0);
+            sigma=sigma+(optionv-value)/vega(spot,strike,time,0,sigma)
         end
         sigmaVec(i)=sigma;    
     end
-    volatility=sigmaVec;
+    %volatility=sigmaVec;
 end
--- 5.option-arbitrage/ExerciseOpt/Template/blackscholes.m
+++ 5.option-arbitrage/ExerciseOpt/Template/blackscholes.m
@@ -1,6 +1,6 @@
 function [call,put] = blackscholes(spot,strike,time,interest,sigma)
-Our own black-scholes formula, without any 
-  Detailed explanation goes here
+%Our own black-scholes formula, without any 
+%  Detailed explanation goes here
 d1=(log(spot/strike)+(interest+sigma^2/2)*time)/(sigma*sqrt(time));
 d2=d1-sigma*sqrt(time);
 call = spot*normcdf(d1)-strike*exp(-interest*time)*normcdf(d2); 
