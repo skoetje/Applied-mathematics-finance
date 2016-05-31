8245a661ebf093a4a6bb8aebd67eaf3e44a64982
Meiden <m.m.dekker@outlook.com>
2016-05-31 14:58:43 +0200

    AnalysisRobot en Plotscripts

--- 5.option-arbitrage/ExerciseOpt/Template/PlotScript.m
+++ 5.option-arbitrage/ExerciseOpt/Template/PlotScript.m
@@ -8,6 +8,7 @@ plot(Timevec,myTradingRobot.StockBidPrice,'-','Color','red','LineWidth',3)
 hold off
 xlabel('Time','FontSize',15)
 ylabel('Price of Stock','FontSize',15)
+legend('Ask','Bid')
 set(gca,'FontSize',13)
 
 %% Plot Ask/Bid stockvolumes
@@ -17,9 +18,10 @@ plot(Timevec,myTradingRobot.StockBidVol,'-','Color','red','LineWidth',3)
 hold off
 xlabel('Time','FontSize',15)
 ylabel('Volume of Stock','FontSize',15)
+legend('Ask','Bid')
 set(gca,'FontSize',13)
 
-%% Plot Ask/Bid optionprices
+%% Plot optionprices
 hold on
 plot(Timevec,myTradingRobot.CallOptionOfferPrice,'-','Color','blue','LineWidth',3)
 plot(Timevec,myTradingRobot.PutOptionOfferPrice,'-','Color','red','LineWidth',3)
@@ -27,14 +29,16 @@ hold off
 xlabel('Time')
 ylabel('Price of Option')
 xlabel('Time','FontSize',15)
-ylabel('Price of Stock','FontSize',15)
+ylabel('Price of Option','FontSize',15)
+legend('Call','Put')
 set(gca,'FontSize',13)
 
-%% Plot Ask/Bid optionvolumes
+%% Plot optionvolumes
 hold on
-plot(Timevec,myTradingRobot.StockOfferVol,'-','Color','blue','LineWidth',3)
-plot(Timevec,myTradingRobot.StockBidVol,'-','Color','red','LineWidth',3)
+plot(Timevec,myTradingRobot.CallOptionOfferVol,'-','Color','blue','LineWidth',3)
+plot(Timevec,myTradingRobot.PutOptionOfferVol,'-','Color','red','LineWidth',3)
 hold off
 xlabel('Time','FontSize',15)
 ylabel('Volume of Stock','FontSize',15)
+legend('Call','Put')
 set(gca,'FontSize',13)
