581de12faeea8f714596f8a5bd1155b85a9ca454
MarkDekker1 <m.m.dekker@outlook.com>
2016-06-02 17:27:57 +0200

    Reordering

--- 5.option-arbitrage/ExerciseOpt/Template/Report.m
+++
@@ -1,4 +0,0 @@
-function Report(aTrades, aSpot)
-    % Report cash and position given the trades and a spot at the time of expiry
-    fprintf('...\n')
-end
--- 5.option-arbitrage/ExerciseOpt/Template/vega.m
+++
@@ -1,5 +0,0 @@
-function vega = vega(spot,strike,time,interest,sigma)
-% Our own black-scholes formula, without any 
-%   Detailed explanation goes here
-d1=(log(spot/strike)+(interest+sigma^2/2)*time)/(sigma*sqrt(time));
-vega = spot*sqrt(1)*normpdf(d1);
