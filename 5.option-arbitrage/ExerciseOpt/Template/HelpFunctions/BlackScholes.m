5f451266f9536bac9f2ffdb14f95412f84012527
Skoetje <b.e.zijlstra@students.uu.nl>
2016-06-06 12:26:56 +0200

    changed bls input sequence (ignore)

--- 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/BlackScholes.m
+++ 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/BlackScholes.m
@@ -1,8 +1,8 @@
-function [call,put] = BlackScholes(S,X,r,T,v)
+function [call,put] = BlackScholes(S,X,T,r,vol)
 % Our own black-scholes formula, without any 
 %   Detailed explanation goes here
-d1=(log(S/X)+(r+v^2/2)*T)/(v*sqrt(T));
-d2=d1-v*sqrt(T);
+d1=(log(S/X)+(r+vol^2/2)*T)/(vol*sqrt(T));
+d2=d1-vol*sqrt(T);
 call = S*normcdf(d1)-X*exp(-r*T)*normcdf(d2);
 put = X*exp(-r*T)*normcdf(-d2)-S*normcdf(-d1);
 end
