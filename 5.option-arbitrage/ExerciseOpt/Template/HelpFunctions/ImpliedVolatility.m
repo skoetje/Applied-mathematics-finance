645cc471d896255f8dcdf4dd1ed2906c3d7b58d2
Skoetje <b.e.zijlstra@students.uu.nl>
2016-06-13 22:43:34 +0200

    updated functions

--- 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/ImpliedVolatility.m
+++ 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/ImpliedVolatility.m
@@ -1,17 +1,32 @@
-function Volatility=ImpliedVolatility(aSpot,aStrike,aTime,aOption,aBoolean)
-%For vector of length 1
+function Volatility=ImpliedVolatility(aSpot,aStrike,aTime,aOption,aIsPut)
 sigma=0.2;
-value=100;
-myExpiry=((169000-aTime)+3600*24*daysact('10-jun-2016',  '16-sep-2016'))/(3600*24*252);
+value=100 + aOption;
+myExpiry=((169000-aTime)+3600*24*daysact('13-jun-2016',  '16-sep-2016'))/(3600*24*252);
 
-while value-aOption>0.01,
+while any(value-aOption > 1e-9)
     [call, put] = BlackScholes(aSpot,aStrike,myExpiry,0,sigma);
-    if aBoolean==1,
-        value=call;
-    else
-        value=put;
-    end
-    sigma=sigma+(aOption-value)/Vega(aSpot,aStrike,aTime,0,sigma);
+    value(aIsPut) = put(aIsPut);
+    value(~aIsPut) = call(~aIsPut);
+    
+    sigma=sigma+(aOption-value)./Vega(aSpot,aStrike,aTime,0,sigma);
 end
 Volatility=sigma;
 end
+
+% function Volatility=ImpliedVolatility(aSpot,aStrike,aTime,aOption,aBoolean)
+% %For vector of length 1
+% sigma=0.2;
+% value=100;
+% myExpiry=((169000-aTime)+3600*24*daysact('10-jun-2016',  '16-sep-2016'))/(3600*24*252);
+% 
+% while value-aOption>0.01,
+%     [call, put] = BlackScholes(aSpot,aStrike,myExpiry,0,sigma);
+%     if aBoolean==1,
+%         value=call;
+%     else
+%         value=put;
+%     end
+%     sigma=sigma+(aOption-value)/Vega(aSpot,aStrike,aTime,0,sigma);
+% end
+% Volatility=sigma;
+% end
--- 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/normProbDensFun.m
+++ 5.option-arbitrage/ExerciseOpt/Template/HelpFunctions/normProbDensFun.m
@@ -6,6 +6,6 @@ function x = normProbDensFun(x)
 % since normally you calculate ((x-mu)/sigma)^2 instead
 % x^2, but simga is always 1 and mu is always 0 in our
 % code.
-x = (1/(1*sqrt(2*pi)))*exp(1)^(-0.5*(x^2));
+x = exp(-0.5*(x.^2)) / sqrt(2*pi);
 end
 
