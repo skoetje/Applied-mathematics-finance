849bb072578ce44c1be04e910c8d9c263048aec8
Meiden <m.m.dekker@outlook.com>
2016-06-14 17:29:25 +0200

    sdfadsf

--- 5.option-arbitrage/ExerciseOpt/Framework/RunExerciseOpt.m
+++ 5.option-arbitrage/ExerciseOpt/Framework/RunExerciseOpt.m
@@ -3,15 +3,7 @@ clear myFeedPublisher;
 clear myOptionsQuoter;
 clear myTradingRobot;
 
-<<<<<<< HEAD
 load('ING1.mat');
-=======
-<<<<<<< HEAD
-load('ING2.mat');
-=======
-load('ING3.mat');
->>>>>>> ab8907186c3711503179a3329ab0f502bc9295c9
->>>>>>> 61eff5b3c907dc206fcb9e112c4eccbd7043741b
 
 myExchange = CreateExchangeOpt();
 
@@ -23,10 +15,10 @@ myOptionsQuoter = OptionsQuoter();
 myExchange.RegisterAutoTrader(myOptionsQuoter);
 myOptionsQuoter.StartAutoTrader(myExchange, myFeedPublisher);
 
-myTradingRobot = AnalysisRobot_final();
+myTradingRobot = TradingRobot();
 myExchange.RegisterAutoTrader(myTradingRobot);
 myTradingRobot.StartAutoTrader(myExchange);
 
 myFeedPublisher.StartVeryShortFeed(myFeed);
 
-Report_Analysis(myTradingRobot);
+Report_Trading(myTradingRobot);
--- 5.option-arbitrage/ExerciseOpt/Template/TradingFunctions/CallPutParityMark.m
+++ 5.option-arbitrage/ExerciseOpt/Template/TradingFunctions/CallPutParityMark.m
@@ -1,9 +1,14 @@
-function CallPutParityMark(aBot,aStrike)
+function CallPutParityMark(aBot,aStrike,aTime)
 
+% Preambule and parameters
 myCallOptionDepth = OptionDepth(aBot,aStrike,1);
 myPutOptionDepth = OptionDepth(aBot,aStrike,0);
 myStrike=aStrike;
-myFactor=1.00;
+myFactor=1.0003;
+myVolumeDivider=1;
+myFeedLength=3300;
+myTimeFactor=1;%abs(myFeedLength-aTime)/myFeedLength;
+myMaximumVolume=6000000;
 
 if isempty(myCallOptionDepth)==0 && isempty(myPutOptionDepth)==0 && isempty(aBot.StockDepth)==0,
     
@@ -22,49 +27,49 @@ if isempty(myCallOptionDepth)==0 && isempty(myPutOptionDepth)==0 && isempty(aBot
         myPutBidP=myPutOptionDepth.bidLimitPrice;
         myPutBidV=myPutOptionDepth.bidVolume;
         mySpotBidP=aBot.StockDepth.bidLimitPrice(1);
-        mySpotBidV=aBot.StockDepth.bidVolume(1);
+        mySpotBidV=round(aBot.StockDepth.bidVolume(1)/myVolumeDivider);
 
-        if myCallAskP+myStrike<(myPutBidP+mySpotBidP)/myFactor;% && myCallPosition<3000 && myPutPosition>-3000;
-            if myCallAskV>0 && myPutBidV >0 && myCallAskV<=myCallOptionDepth.askVolume && myPutBidV<=myPutOptionDepth.askVolume, 
+        if myCallAskP+myStrike<(myPutBidP+mySpotBidP)/myFactor && myCallPosition<myMaximumVolume*myTimeFactor && myPutPosition>-myMaximumVolume*myTimeFactor;
+            if myCallAskV>0 && myPutBidV >0 && mySpotBidV>0 && myCallAskV<=myCallOptionDepth.askVolume && myPutBidV<=myPutOptionDepth.askVolume, 
+                
+                myTradeVol=min([myCallAskV,myPutBidV,aBot.StockDepth.bidVolume(1)]);
+                
+                aBot.StockDepth.bidVolume(1) = aBot.StockDepth.bidVolume(1)-myTradeVol;
+                aBot.SendNewOrder(aBot.StockDepth.bidLimitPrice(1), myTradeVol,  -1, {'ING'}, {'IMMEDIATE'}, 0);  
                 
                 if myStrike==8, 
-                    aBot.Call800Depth.askVolume = aBot.Call800Depth.askVolume-myCallAskV;   
-                    aBot.Put800Depth.bidVolume = aBot.Put800Depth.bidVolume-myPutBidV;
+                    aBot.Call800Depth.askVolume = aBot.Call800Depth.askVolume-myTradeVol;   
+                    aBot.Put800Depth.bidVolume = aBot.Put800Depth.bidVolume-myTradeVol;
                 elseif myStrike==9,
-                    aBot.Call900Depth.askVolume = aBot.Call900Depth.askVolume-myCallAskV;   
-                    aBot.Put900Depth.bidVolume = aBot.Put900Depth.bidVolume-myPutBidV; 
+                    aBot.Call900Depth.askVolume = aBot.Call900Depth.askVolume-myTradeVol;   
+                    aBot.Put900Depth.bidVolume = aBot.Put900Depth.bidVolume-myTradeVol; 
                 elseif myStrike==9.5,
-                    aBot.Call950Depth.askVolume = aBot.Call950Depth.askVolume-myCallAskV;   
-                    aBot.Put950Depth.bidVolume = aBot.Put950Depth.bidVolume-myPutBidV; 
+                    aBot.Call950Depth.askVolume = aBot.Call950Depth.askVolume-myTradeVol;   
+                    aBot.Put950Depth.bidVolume = aBot.Put950Depth.bidVolume-myTradeVol; 
                 elseif myStrike==9.75,
-                    aBot.Call975Depth.askVolume = aBot.Call975Depth.askVolume-myCallAskV;   
-                    aBot.Put975Depth.bidVolume = aBot.Put975Depth.bidVolume-myPutBidV; 
+                    aBot.Call975Depth.askVolume = aBot.Call975Depth.askVolume-myTradeVol;   
+                    aBot.Put975Depth.bidVolume = aBot.Put975Depth.bidVolume-myTradeVol; 
                 elseif myStrike==10,
-                    aBot.Call1000Depth.askVolume = aBot.Call1000Depth.askVolume-myCallAskV;   
-                    aBot.Put1000Depth.bidVolume = aBot.Put1000Depth.bidVolume-myPutBidV; 
+                    aBot.Call1000Depth.askVolume = aBot.Call1000Depth.askVolume-myTradeVol;   
+                    aBot.Put1000Depth.bidVolume = aBot.Put1000Depth.bidVolume-myTradeVol; 
                 elseif myStrike==10.25,
-                    aBot.Call1025Depth.askVolume = aBot.Call1025Depth.askVolume-myCallAskV;   
-                    aBot.Put1025Depth.bidVolume = aBot.Put1025Depth.bidVolume-myPutBidV; 
+                    aBot.Call1025Depth.askVolume = aBot.Call1025Depth.askVolume-myTradeVol;   
+                    aBot.Put1025Depth.bidVolume = aBot.Put1025Depth.bidVolume-myTradeVol; 
                 elseif myStrike==10.50,
-                    aBot.Call1050Depth.askVolume = aBot.Call1050Depth.askVolume-myCallAskV;   
-                    aBot.Put1050Depth.bidVolume = aBot.Put1050Depth.bidVolume-myPutBidV; 
+                    aBot.Call1050Depth.askVolume = aBot.Call1050Depth.askVolume-myTradeVol;   
+                    aBot.Put1050Depth.bidVolume = aBot.Put1050Depth.bidVolume-myTradeVol; 
                 elseif myStrike==11,
-                    aBot.Call1100Depth.askVolume = aBot.Call1100Depth.askVolume-myCallAskV;   
-                    aBot.Put1100Depth.bidVolume = aBot.Put1100Depth.bidVolume-myPutBidV; 
+                    aBot.Call1100Depth.askVolume = aBot.Call1100Depth.askVolume-myTradeVol;   
+                    aBot.Put1100Depth.bidVolume = aBot.Put1100Depth.bidVolume-myTradeVol; 
                 elseif myStrike==12,
-                    aBot.Call1200Depth.askVolume = aBot.Call1200Depth.askVolume-myCallAskV;   
-                    aBot.Put1200Depth.bidVolume = aBot.Put1200Depth.bidVolume-myPutBidV; 
+                    aBot.Call1200Depth.askVolume = aBot.Call1200Depth.askVolume-myTradeVol;   
+                    aBot.Put1200Depth.bidVolume = aBot.Put1200Depth.bidVolume-myTradeVol; 
                 elseif myStrike==14,
-                    aBot.Call1400Depth.askVolume = aBot.Call1400Depth.askVolume-myCallAskV;   
-                    aBot.Put1400Depth.bidVolume = aBot.Put1400Depth.bidVolume-myPutBidV; 
+                    aBot.Call1400Depth.askVolume = aBot.Call1400Depth.askVolume-myTradeVol;   
+                    aBot.Put1400Depth.bidVolume = aBot.Put1400Depth.bidVolume-myTradeVol; 
                 end
-                %aBot.StockDepth.bidVolume = aBot.StockDepth.bidVolume - 
-                
-                aBot.SendNewOrder(100, myCallAskV,  1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 1);
-                aBot.SendNewOrder(0.01, myPutBidV,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 2);
-                %aBot.SendNewOrder(mySpotBidP, mySpotBidV,  -1, {'ING'}, {'IMMEDIATE'}, 0);
-                %aBot.SendNewOrder(100, 2,  1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 0);
-                %aBot.SendNewOrder(0.01, 2,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 0);
+                aBot.SendNewOrder(100, myTradeVol,  1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 1);
+                aBot.SendNewOrder(0.01, myTradeVol,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 2);
             end
         end
     end
@@ -78,68 +83,70 @@ if isempty(myCallOptionDepth)==0 && isempty(myPutOptionDepth)==0 && isempty(aBot
         myPutAskP=myPutOptionDepth.askLimitPrice;
         myPutAskV=myPutOptionDepth.askVolume;
         mySpotAskP=aBot.StockDepth.askLimitPrice(1);
-        mySpotAskV=aBot.StockDepth.askVolume(1);
+        mySpotAskV=round(aBot.StockDepth.askVolume(1)/myVolumeDivider);
         
         if myCallBidP+myStrike>(myPutAskP+mySpotAskP)*myFactor,
-        if myPutPosition<10000000000;% && myCallPosition>-3000;        
-            if myCallBidV>0 && myPutAskV >0 && myCallBidV<=myCallOptionDepth.bidVolume && myPutAskV<=myPutOptionDepth.askVolume,
+        if myPutPosition<myMaximumVolume*myTimeFactor && myCallPosition>-myMaximumVolume*myTimeFactor;        
+            if myCallBidV>0 && myPutAskV >0 && mySpotAskV>0 && myCallBidV<=myCallOptionDepth.bidVolume && myPutAskV<=myPutOptionDepth.askVolume,
+                
+                myTradeVol=min([myCallBidV,myPutAskV,aBot.StockDepth.askVolume(1)]);
+                
+                aBot.StockDepth.askVolume(1) = aBot.StockDepth.askVolume(1)-myTradeVol;
+                aBot.SendNewOrder(aBot.StockDepth.askLimitPrice(1), myTradeVol,  1, {'ING'}, {'IMMEDIATE'}, 0);
                 
                 if myStrike==8, 
                     myBidVolume=aBot.Call800Depth.bidVolume;
                     myAskVolume=aBot.Put800Depth.askVolume;
-                    aBot.Call800Depth.bidVolume = aBot.Call800Depth.bidVolume-myCallBidV;   
-                    aBot.Put800Depth.askVolume = aBot.Put800Depth.askVolume-myPutAskV;
+                    aBot.Call800Depth.bidVolume = aBot.Call800Depth.bidVolume-myTradeVol;   
+                    aBot.Put800Depth.askVolume = aBot.Put800Depth.askVolume-myTradeVol;
                 elseif myStrike==9,
                     myBidVolume=aBot.Call900Depth.bidVolume;
                     myAskVolume=aBot.Put900Depth.askVolume;
-                    aBot.Call900Depth.bidVolume = aBot.Call900Depth.bidVolume-myCallBidV;   
-                    aBot.Put900Depth.askVolume = aBot.Put900Depth.askVolume-myPutAskV; 
+                    aBot.Call900Depth.bidVolume = aBot.Call900Depth.bidVolume-myTradeVol;   
+                    aBot.Put900Depth.askVolume = aBot.Put900Depth.askVolume-myTradeVol; 
                 elseif myStrike==9.5,
                     myBidVolume=aBot.Call950Depth.bidVolume;
                     myAskVolume=aBot.Put950Depth.askVolume;
-                    aBot.Call950Depth.bidVolume = aBot.Call950Depth.bidVolume-myCallBidV;   
-                    aBot.Put950Depth.askVolume = aBot.Put950Depth.askVolume-myPutAskV; 
+                    aBot.Call950Depth.bidVolume = aBot.Call950Depth.bidVolume-myTradeVol;   
+                    aBot.Put950Depth.askVolume = aBot.Put950Depth.askVolume-myTradeVol; 
                 elseif myStrike==9.75,
                     myBidVolume=aBot.Call975Depth.bidVolume;
                     myAskVolume=aBot.Put975Depth.askVolume;
-                    aBot.Call975Depth.bidVolume = aBot.Call975Depth.bidVolume-myCallBidV;   
-                    aBot.Put975Depth.askVolume = aBot.Put975Depth.askVolume-myPutAskV; 
+                    aBot.Call975Depth.bidVolume = aBot.Call975Depth.bidVolume-myTradeVol;   
+                    aBot.Put975Depth.askVolume = aBot.Put975Depth.askVolume-myTradeVol; 
                 elseif myStrike==10,
                     myBidVolume=aBot.Call1000Depth.bidVolume;
                     myAskVolume=aBot.Put1000Depth.askVolume;
-                    aBot.Call1000Depth.bidVolume = aBot.Call1000Depth.bidVolume-myCallBidV;   
-                    aBot.Put1000Depth.askVolume = aBot.Put1000Depth.askVolume-myPutAskV; 
+                    aBot.Call1000Depth.bidVolume = aBot.Call1000Depth.bidVolume-myTradeVol;   
+                    aBot.Put1000Depth.askVolume = aBot.Put1000Depth.askVolume-myTradeVol; 
                 elseif myStrike==10.25,
                     myBidVolume=aBot.Call1025Depth.bidVolume;
                     myAskVolume=aBot.Put1025Depth.askVolume;
-                    aBot.Call1025Depth.bidVolume = aBot.Call1025Depth.bidVolume-myCallBidV;   
-                    aBot.Put1025Depth.askVolume = aBot.Put1025Depth.askVolume-myPutAskV; 
+                    aBot.Call1025Depth.bidVolume = aBot.Call1025Depth.bidVolume-myTradeVol;   
+                    aBot.Put1025Depth.askVolume = aBot.Put1025Depth.askVolume-myTradeVol; 
                 elseif myStrike==10.50,
                     myBidVolume=aBot.Call1050Depth.bidVolume;
                     myAskVolume=aBot.Put1050Depth.askVolume;
-                    aBot.Call1050Depth.bidVolume = aBot.Call1050Depth.bidVolume-myCallBidV;   
-                    aBot.Put1050Depth.askVolume = aBot.Put1050Depth.askVolume-myPutAskV; 
+                    aBot.Call1050Depth.bidVolume = aBot.Call1050Depth.bidVolume-myTradeVol;   
+                    aBot.Put1050Depth.askVolume = aBot.Put1050Depth.askVolume-myTradeVol; 
                 elseif myStrike==11,
                     myBidVolume=aBot.Call1100Depth.bidVolume;
                     myAskVolume=aBot.Put1100Depth.askVolume;
-                    aBot.Call1100Depth.bidVolume = aBot.Call1100Depth.bidVolume-myCallBidV;   
-                    aBot.Put1100Depth.askVolume = aBot.Put1100Depth.askVolume-myPutAskV; 
+                    aBot.Call1100Depth.bidVolume = aBot.Call1100Depth.bidVolume-myTradeVol;   
+                    aBot.Put1100Depth.askVolume = aBot.Put1100Depth.askVolume-myTradeVol; 
                 elseif myStrike==12,
                     myBidVolume=aBot.Call1200Depth.bidVolume;
                     myAskVolume=aBot.Put1200Depth.askVolume;
-                    aBot.Call1200Depth.bidVolume = aBot.Call1200Depth.bidVolume-myCallBidV;   
-                    aBot.Put1200Depth.askVolume = aBot.Put1200Depth.askVolume-myPutAskV; 
+                    aBot.Call1200Depth.bidVolume = aBot.Call1200Depth.bidVolume-myTradeVol;   
+                    aBot.Put1200Depth.askVolume = aBot.Put1200Depth.askVolume-myTradeVol; 
                 elseif myStrike==14,
                     myBidVolume=aBot.Call1400Depth.bidVolume;
                     myAskVolume=aBot.Put1400Depth.askVolume;
-                    aBot.Call1400Depth.bidVolume = aBot.Call1400Depth.bidVolume-myCallBidV;   
-                    aBot.Put1400Depth.askVolume = aBot.Put1400Depth.askVolume-myPutAskV; 
+                    aBot.Call1400Depth.bidVolume = aBot.Call1400Depth.bidVolume-myTradeVol;   
+                    aBot.Put1400Depth.askVolume = aBot.Put1400Depth.askVolume-myTradeVol; 
                 end
-                                
-                aBot.SendNewOrder(0.01, myCallBidV,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 3);
-                aBot.SendNewOrder(100, myPutAskV,  1, {myPutOptionDepth.ISIN}, {'IMMEDIATE'}, 4);
-                %aBot.SendNewOrder(0.01, myBidVolume,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 0);
-                %aBot.SendNewOrder(100, myAskVolume,  1, {myPutOptionDepth.ISIN}, {'IMMEDIATE'}, 0);
+                aBot.SendNewOrder(0.01, myTradeVol,  -1, {myCallOptionDepth.ISIN}, {'IMMEDIATE'}, 3);
+                aBot.SendNewOrder(100, myTradeVol,  1, {myPutOptionDepth.ISIN}, {'IMMEDIATE'}, 4);
             end
         end
         end
