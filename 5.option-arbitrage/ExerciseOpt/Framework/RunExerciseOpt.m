fbce5230ac6c8b8aa8e5cbee21b67900bff197f7
MarkDekker1 <m.m.dekker@outlook.com>
2016-06-14 18:12:37 +0200

    Better

--- 5.option-arbitrage/ExerciseOpt/Framework/RunExerciseOpt.m
+++ 5.option-arbitrage/ExerciseOpt/Framework/RunExerciseOpt.m
@@ -3,26 +3,21 @@ clear myFeedPublisher;
 clear myOptionsQuoter;
 clear myTradingRobot;
 
-<<<<<<< HEAD
 load('ING1.mat');
-
-=======
-load('ING2.mat');
->>>>>>> 015bdc68c6ae3867e1825eee993bb73ed65f9e46
 myExchange = CreateExchangeOpt();
 
 myFeedPublisher = FeedPublisher();
 myExchange.RegisterAutoTrader(myFeedPublisher);
-myFeedPublisher.StartAutoT rader(myExchange);
+myFeedPublisher.StartAutoTrader(myExchange);
 
 myOptionsQuoter = OptionsQuoter();
 myExchange.RegisterAutoTrader(myOptionsQuoter);
 myOptionsQuoter.StartAutoTrader(myExchange, myFeedPublisher);
 
-myTradingRobot = TradingRobot();
+myTradingRobot = AnalysisRobot_final();
 myExchange.RegisterAutoTrader(myTradingRobot);
 myTradingRobot.StartAutoTrader(myExchange);
 
-myFeedPublisher.StartFeed(myFeed);
+myFeedPublisher.StartVeryShortFeed(myFeed);
 
-Report_Trading(myTradingRobot);
+Report_Analysis(myTradingRobot);
